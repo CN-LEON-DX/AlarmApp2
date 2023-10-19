cb18a25599ec63fb4ef9fee08c35e7196f40e2ea
FuzzilFox144 <chinh9675pl365@gmail.com>
2023-10-19 15:19:18 +0700

    edited by chinh nguyen 17/10/2023

--- app/src/main/java/com/example/alarmapp/alarm_pack/Editting_Alarm_Activity.java
+++ app/src/main/java/com/example/alarmapp/alarm_pack/Editting_Alarm_Activity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import com.example.alarmapp.R;
 
 public class Editting_Alarm_Activity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
